44891cca9c8e1dd13fbbd115ecb60e8fd1f15b18
siaheguelable <siaheguelable@gmail.com>
2025-08-11 22:26:51 +0200

    asigment 6 done

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,15 +13,19 @@ public class Node
     {
         // TODO Start Problem 1
 
+
+
         if (value < Data)
         {
+
+
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > data)
         {
             // Insert to the right
             if (Right is null)
@@ -29,12 +33,48 @@ public class Node
             else
                 Right.Insert(value);
         }
+
+        else
+        // value == this.Value → duplicate, so do nothing
+        {
+            return;
+        }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+
+        if (value==data)
+        //If they are equal → return true (found it)
+        {
+            return true;
+        }
+
+        if (value< data)
+        //If the target is less than the current node’s value → search left.
+        {
+            // search to the left
+            if (Left!=null )
+        
+                return left.Contains(value);
+            
+            else
+            
+                return false; // // No left child, not found
+        }
+
+       else
+       {
+         // Go right if possible
+        if (this.Right != null)
+            return this.Right.Contains(value);
+        else
+            return false; // No right child, not found
+
+       }
+
+        
     }
 
     public int GetHeight()
--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -49,5 +49,23 @@ public static class Trees
     private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
     {
         // TODO Start Problem 5
+
+        if (first> last)
+        return; // Base case: no more elements in this range
+
+        // Find the middle index
+        int mid=(first +last)/2
+
+        // insert the middle element into the tree
+        tree.Insert(sorted_list[mid]);
+
+
+        // Recursively insert the left half
+        InsertMiddle(tree, sortedList, first, mid - 1);
+
+        // Recursively insert the right half
+        InsertMiddle(tree, sortedList, mid + 1, last);
+
+
     }
 }
